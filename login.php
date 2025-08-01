1e2b369936671445c633f9689720fd0c7399deb8
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-02 00:09:48 +0600

    2nd commit

--- login.php
+++ login.php
@@ -50,10 +50,10 @@ if(isset($_POST['submit'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>login</title>
 
-   <!-- font awesome cdn link  -->
+  
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-   <!-- custom css file link  -->
+  
    <link rel="stylesheet" href="css/components.css">
 
 </head>
@@ -80,7 +80,7 @@ if(isset($message)){
       <h3>login now</h3>
       <input type="email" name="email" class="box" placeholder="enter your email" required>
       <input type="password" name="pass" class="box" placeholder="enter your password" required>
-      <input type="submit" value="login now" class="btn" name="submit">
+      <input type="submit" value="Login now" class="btn" name="Submit">
       <p>don't have an account? <a href="register.php">register now</a></p>
    </form>
 
